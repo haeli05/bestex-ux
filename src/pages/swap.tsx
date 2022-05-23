b20df52cc68a25be0506f8a7f325742eb9a934b8
GLaDOS III <gladosiii@GLaDOSs-MacBook-Pro.local>
2022-05-22 21:19:46 -0400

    jupiter benchmark component

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -9,6 +9,8 @@ import useNotification from '@/application/notification/useNotification'
 import { routeTo } from '@/application/routeTools'
 import { getCoingeckoChartPriceData } from '@/application/swap/klinePrice'
 import txSwap from '@/application/swap/txSwap'
+import txUnwrapAllWSOL, { txUnwrapWSOL } from '@/application/swap/txUnwrapWSOL'
+import txWrapSOL from '@/application/swap/txWrapSOL'
 import { useSwap } from '@/application/swap/useSwap'
 import { useSwapAmountCalculator } from '@/application/swap/useSwapAmountCalculator'
 import useSwapInitCoinFiller from '@/application/swap/useSwapInitCoinFiller'
@@ -16,15 +18,11 @@ import useSwapUrlParser from '@/application/swap/useSwapUrlParser'
 import { SplToken } from '@/application/token/type'
 import useToken, { RAYDIUM_MAINNET_TOKEN_LIST_NAME } from '@/application/token/useToken'
 import {
-  SOL_BASE_BALANCE,
-  SOLDecimals,
-  WSOLMint,
-  isQuantumSOLVersionSOL,
-  isQuantumSOLVersionWSOL,
-  toUITokenAmount
+  isQuantumSOLVersionSOL, isQuantumSOLVersionWSOL, SOL_BASE_BALANCE, SOLDecimals, toUITokenAmount, WSOLMint
 } from '@/application/token/utils/quantumSOL'
 import { USDCMint, USDTMint } from '@/application/token/utils/wellknownToken.config'
 import useWallet from '@/application/wallet/useWallet'
+import { Badge } from '@/components/Badge'
 import Button, { ButtonHandle } from '@/components/Button'
 import Card from '@/components/Card'
 import { Checkbox } from '@/components/Checkbox'
@@ -49,6 +47,7 @@ import formatNumber from '@/functions/format/formatNumber'
 import toPubString from '@/functions/format/toMintString'
 import toPercentString from '@/functions/format/toPercentString'
 import { toTokenAmount } from '@/functions/format/toTokenAmount'
+import { isMintEqual } from '@/functions/judgers/areEqual'
 import { eq, gte, isMeaningfulNumber, isMeaninglessNumber, lt, lte } from '@/functions/numberish/compare'
 import { div, mul } from '@/functions/numberish/operations'
 import { toString } from '@/functions/numberish/toString'
@@ -60,10 +59,8 @@ import useToggle from '@/hooks/useToggle'
 import { HexAddress, Numberish } from '@/types/constants'
 
 import { useSwapTwoElements } from '../hooks/useSwapTwoElements'
-import { Badge } from '@/components/Badge'
-import txUnwrapAllWSOL, { txUnwrapWSOL } from '@/application/swap/txUnwrapWSOL'
-import { isMintEqual } from '@/functions/judgers/areEqual'
-import txWrapSOL from '@/application/swap/txWrapSOL'
+
+import JupiterComponent from './JupiterComponent'
 
 function SwapEffect() {
   useSwapInitCoinFiller()
@@ -357,7 +354,6 @@ function SwapCard() {
       </FadeInStable>
       {/* alert user if price has accidently change  */}
       <SwapPriceAcceptChip />
-
       {/* swap sol and wsol */}
       {isSolToWsol(upCoin, downCoin) || isWsolToSol(upCoin, downCoin) ? (
         <Button
@@ -481,6 +477,8 @@ function SwapCard() {
         }}
         close={turnOffCoinSelector}
       />
+      {/* Jupiter Benchmark */}
+      <JupiterComponent />
     </CyberpunkStyleCard>
   )
 
